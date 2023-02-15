3d71a43ee14102bc5d540ffe6c281c3090ec298f
frank <frank@parity.io>
2023-02-15 15:57:11 +0000

    test: implement test for tip dispatchable function

---
+++ src/tests/autopay.rs
@@ -0,0 +1,90 @@
+use super::*;
+use crate::types::QueryDataOf;
+use frame_support::{
+	assert_noop, assert_ok,
+	traits::{fungible::Inspect, Currency},
+};
+use sp_core::keccak_256;
+
+#[test]
+fn tips() {
+	// Based on https://github.com/tellor-io/autoPay/blob/b0eca105f536d7fd6046cf1f53125928839a3bb0/test/functionTests-TellorAutopay.js#L199
+	new_test_ext().execute_with(|| {
+		register(STAKE_AMOUNT);
+
+		let reporter = 2;
+		deposit_stake(reporter, STAKE_AMOUNT, Address::random());
+
+		let tipper = 1;
+		let query_data: QueryDataOf<Test> = spot_price("dot", "usd").try_into().unwrap();
+		let query_id = keccak_256(query_data.as_ref()).into();
+		let amount = 100;
+
+		assert_noop!(
+			Tellor::tip(RuntimeOrigin::signed(tipper), H256::random(), amount, query_data.clone()),
+			Error::InvalidQueryId
+		);
+		assert_noop!(
+			Tellor::tip(RuntimeOrigin::signed(tipper), query_id, 0, query_data.clone()),
+			Error::InvalidAmount
+		);
+		assert_noop!(
+			Tellor::tip(RuntimeOrigin::signed(tipper), query_id, amount, query_data.clone()),
+			<pallet_balances::Error<Test>>::InsufficientBalance
+		);
+
+		Balances::make_free_balance_be(&tipper, 1000);
+		assert_ok!(Tellor::tip(
+			RuntimeOrigin::signed(tipper),
+			query_id,
+			amount,
+			query_data.clone()
+		));
+		assert_eq!(Tellor::get_current_tip(query_id), amount, "tip 1 should be correct");
+		assert_eq!(Balances::balance(&tipper), 900);
+		assert_ok!(Tellor::submit_value(
+			RuntimeOrigin::signed(reporter),
+			query_id,
+			uint_value(3550),
+			0,
+			query_data.clone()
+		));
+
+		next_block(); // required for a timestamp that is newer than last submitted value
+
+		assert_ok!(Tellor::tip(RuntimeOrigin::signed(tipper), query_id, 200, query_data.clone()));
+		assert_eq!(Tellor::get_current_tip(query_id), 200, "tip 2 should be correct");
+		assert_eq!(Balances::balance(&tipper), 700);
+		assert_ok!(Tellor::tip(RuntimeOrigin::signed(tipper), query_id, 300, query_data.clone()));
+		assert_eq!(Tellor::get_current_tip(query_id), 500, "tip 3 should be correct");
+		assert_eq!(Balances::balance(&tipper), 400);
+
+		// test query data storage
+		assert_eq!(
+			Tellor::get_query_data(query_id).unwrap(),
+			query_data,
+			"query data not stored correctly"
+		);
+		let query_data: QueryDataOf<Test> = spot_price("btc", "usd").try_into().unwrap();
+		let query_id = keccak_256(query_data.as_ref()).into();
+		assert_ok!(Tellor::tip(RuntimeOrigin::signed(tipper), query_id, 10, query_data.clone()));
+		assert_eq!(
+			Tellor::get_query_data(query_id).unwrap(),
+			query_data,
+			"query data not stored correctly"
+		);
+		assert_ok!(Tellor::submit_value(
+			RuntimeOrigin::signed(reporter),
+			query_id,
+			uint_value(3550),
+			0,
+			query_data.clone()
+		));
+		assert_ok!(Tellor::tip(RuntimeOrigin::signed(tipper), query_id, 10, query_data.clone()));
+		assert_eq!(
+			Tellor::get_query_data(query_id).unwrap(),
+			query_data,
+			"query data not stored correctly"
+		);
+	});
+}
---
+++ src/tests/mod.rs
@@ -0,0 +1,110 @@
+use crate::{
+	mock::*,
+	types::{AccountIdOf, Address, Amount, AmountOf, QueryDataOf, ValueOf},
+	Event, Origin,
+};
+use ethabi::{Bytes, Token, Uint};
+use frame_support::{assert_ok, traits::PalletInfoAccess};
+use sp_core::{bytes::to_hex, keccak_256, H256};
+use xcm::prelude::{DescendOrigin, PalletInstance, X1};
+
+mod autopay;
+
+type Error = crate::Error<Test>;
+
+#[test]
+fn reports_stake_deposited() {
+	new_test_ext().execute_with(|| {
+		System::set_block_number(1);
+
+		let reporter = 1;
+		let amount: Amount = 42.into();
+		let address = Address::random();
+		assert_ok!(Tellor::report_stake_deposited(
+			Origin::Staking.into(),
+			reporter,
+			amount,
+			address
+		));
+
+		// // Read pallet storage and assert an expected result.
+		// assert_eq!(TemplateModule::something(), Some(42));
+
+		System::assert_last_event(
+			Event::NewStakerReported { staker: reporter, amount: amount.low_u64(), address }.into(),
+		);
+	});
+}
+
+#[test]
+fn begins_dispute() {
+	new_test_ext().execute_with(|| {
+		register(STAKE_AMOUNT);
+
+		let reporter = 1;
+		deposit_stake(reporter, STAKE_AMOUNT, Address::random());
+
+		let query_data: QueryDataOf<Test> = spot_price("dot", "usd").try_into().unwrap();
+		let query_id = keccak_256(query_data.as_ref()).into();
+		assert_ok!(Tellor::submit_value(
+			RuntimeOrigin::signed(reporter),
+			query_id,
+			uint_value(123),
+			0,
+			query_data
+		));
+
+		let timestamp = Timestamp::now();
+		assert_ok!(Tellor::begin_dispute(RuntimeOrigin::signed(reporter), query_id, timestamp));
+
+		let sent_messages = sent_xcm();
+		let (_, sent_message) = sent_messages.first().unwrap();
+		assert!(sent_message
+			.0
+			.contains(&DescendOrigin(X1(PalletInstance(Tellor::index() as u8)))));
+		// todo: check remaining instructions
+
+		// // Read pallet storage and assert an expected result.
+		// assert_eq!(TemplateModule::something(), Some(42));
+
+		System::assert_last_event(
+			Event::NewDispute { dispute_id: 0, query_id, timestamp, reporter }.into(),
+		);
+	});
+}
+
+fn deposit_stake(reporter: AccountIdOf<Test>, amount: impl Into<Amount>, address: Address) {
+	assert_ok!(Tellor::report_stake_deposited(
+		Origin::Staking.into(),
+		reporter,
+		amount.into(),
+		address
+	));
+}
+
+const STAKE_AMOUNT: AmountOf<Test> = 100;
+fn register(stake_amount: AmountOf<Test>) {
+	assert_ok!(Tellor::register(RuntimeOrigin::root(), stake_amount, 1000, 1000));
+}
+
+fn spot_price(asset: impl Into<String>, currency: impl Into<String>) -> Bytes {
+	ethabi::encode(&[
+		Token::String("SpotPrice".to_string()),
+		Token::Bytes(ethabi::encode(&[
+			Token::String(asset.into()),
+			Token::String(currency.into()),
+		])),
+	])
+}
+
+fn uint_value(value: impl Into<Uint>) -> ValueOf<Test> {
+	ethabi::encode(&[Token::Uint(value.into())]).try_into().unwrap()
+}
+
+#[test]
+fn encodes_spot_price() {
+	assert_eq!(
+		"0xa6f013ee236804827b77696d350e9f0ac3e879328f2a3021d473a0b778ad78ac",
+		to_hex(&keccak_256(&spot_price("btc", "usd")), false)
+	)
+}
