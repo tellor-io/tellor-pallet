867a6c9d082f4caa3268697d91f52a0b84242951
frank <frank@parity.io>
2023-03-13 12:51:08 +0000

    test(governance): add test list

---
+++ src/tests/governance.rs
@@ -0,0 +1,77 @@
+#[test]
+#[ignore]
+fn begin_dispute() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn execute_vote() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn tally_votes() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn vote() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn vote_on_multiple_disputes() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn did_vote() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn get_dispute_info() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn get_open_disputes_on_id() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn get_vote_count() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn get_vote_info() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn get_vote_rounds() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn get_vote_tally_by_address() {
+	todo!()
+}
+
+#[test]
+#[ignore]
+fn get_user_tips() {
+	todo!()
+}
