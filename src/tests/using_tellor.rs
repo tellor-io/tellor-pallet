55fb52fe6d50f006f054bfd84b583b40e066aa63
Frank Bell <frank@parity.io>
2023-04-30 18:02:48 +0100

    test: add get_index_for_data_before test

--- src/tests/using_tellor.rs
+++ src/tests/using_tellor.rs
@@ -38,9 +38,53 @@ fn get_timestamp_by_query_id_and_index() {
 }
 
 #[test]
-#[ignore]
 fn get_index_for_data_before() {
-	todo!()
+	let query_data: QueryDataOf<Test> = spot_price("dot", "usd").try_into().unwrap();
+	let query_id = keccak_256(query_data.as_ref()).into();
+	let reporter = 1;
+
+	let mut ext = new_test_ext();
+
+	// Prerequisites
+	ext.execute_with(|| {
+		with_block(|| deposit_stake(reporter, MINIMUM_STAKE_AMOUNT, Address::random()))
+	});
+
+	// Based on https://github.com/tellor-io/usingtellor/blob/cfc56240e0f753f452d2f376b5ab126fa95222ad/test/functionTests-UsingTellor.js#L68
+	ext.execute_with(|| {
+		with_block(|| {
+			assert_ok!(Tellor::submit_value(
+				RuntimeOrigin::signed(reporter),
+				query_id,
+				uint_value(150),
+				0,
+				query_data.clone(),
+			));
+			now()
+		});
+		with_block_after(REPORTING_LOCK, || {
+			assert_ok!(Tellor::submit_value(
+				RuntimeOrigin::signed(reporter),
+				query_id,
+				uint_value(160),
+				1,
+				query_data.clone(),
+			));
+			now()
+		});
+		let timestamp_3 = with_block_after(REPORTING_LOCK, || {
+			assert_ok!(Tellor::submit_value(
+				RuntimeOrigin::signed(reporter),
+				query_id,
+				uint_value(170),
+				2,
+				query_data.clone(),
+			));
+			now()
+		});
+
+		assert_eq!(Tellor::get_index_for_data_before(query_id, timestamp_3), Some(1))
+	});
 }
 
 #[test]
